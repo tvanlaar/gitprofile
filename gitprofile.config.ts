5903e7d93aff57d73dfddf3bb373a4c7b717599f
Tricia Van Laar <tvanlaar@csustan.edu>
2024-04-11 11:30:35 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -37,7 +37,7 @@ const CONFIG = {
           description:
             '3 minute presentation of my research.',
           imageUrl:
-            'https://github.com/tvanlaar/gitprofile/blob/main/img/VanLaarRSCASlide.jpg',
+            'https://github.com/tvanlaar/gitprofile/raw/main/img/VanLaarRSCASlide.jpg',
           link: 'https://github.com/tvanlaar/gitprofile/blob/main/img/VanLaarRSCASlide.jpg',
         },
         {
